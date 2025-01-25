efbb5fb6c761beb2f367b91606f2157875fbcd60
RhexNiebres <rhexniebres@gmail.com>
2025-01-24 17:10:14 -0800

    add file to test data transfer in React

---
+++ my-first-react-app/src/Button.jsx
@@ -0,0 +1,14 @@
+function Button(){
+    return(
+        <button>Cllick me!</button>
+    )
+}
+
+export default function RenderButtons(){
+    return(
+        <div>
+            <Button/>
+            <Button/>
+        </div>
+    )
+}
