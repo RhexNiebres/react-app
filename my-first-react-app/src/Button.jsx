ff7d9c4c0eb29776e98e9c5a8a8a44b649fd5a7b
RhexNiebres <rhexniebres@gmail.com>
2025-01-25 13:23:39 -0800

    feat add handleButtonClick and pass it as value to reference this function to  handleClick propr in Button component

--- my-first-react-app/src/Button.jsx
+++ my-first-react-app/src/Button.jsx
@@ -1,18 +1,21 @@
-function Button({text = 'Click Me!', color = 'lightgreen', fontSize=12}){
+function Button({text = 'Click Me!', color = 'lightgreen', fontSize=12, handleClick}){
     const buttonStyle= {
         color: color,
         fontSize: fontSize + 'px',
     };
     
     return(
-        <button style={buttonStyle}>{text}</button>
+        <button onClick={handleClick} style={buttonStyle}>{text}</button>
     )
 }
 
 export default function RenderButtons(){
+    const handleButtonClick= (url) => {
+       window.location.href = url;
+    }
     return(
         <div>
-             <Button  />
+             <Button text="Google" handleClick={()=> handleButtonClick('https://www.google.com')}  />
              <Button text="Don't Click Me!" color="red" fontSize={12} />
              <Button color="orange" fontSize={12} />
         </div>
