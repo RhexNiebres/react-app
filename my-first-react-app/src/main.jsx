86b638df1339c83cb08c1c28885f8b966b54b894
RhexNiebres <rhexniebres@gmail.com>
2025-01-24 17:09:47 -0800

    update import  RenderButtons   component to be render

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx' //replaced by Greeting
-import Greeting from './Greeting.jsx'
+// import Greeting from './Greeting.jsx'
+import RenderButtons from './Button.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Greeting />
+    < RenderButtons />
   </StrictMode>,
 )
